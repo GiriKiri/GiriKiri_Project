d3cc6d8994a2401c682f6e83aeb6af7c3c6008c1
g2Min <jimin9320@gmail.com>
2024-05-07 04:35:27 +0900

    feat: 기본적인 모델 추출완료

--- GiriProject/detect.py
+++ GiriProject/detect.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
 # load a pretrained Yolov8n model
-model = YOLO('best.pt')
-
+# 이 부분은 무조건 절대경로로 설정해야함.
+model_path = "C:/Users/Jimin Lee/OneDrive/바탕 화면/AIproject/2023-02-Giri/GiriProject/best.pt"
+#model = YOLO(model_path)
+model = YOLO('yolov5s.pt')
 # run inference on the source
-results = model(source=1, show=True, conf=0.4, save=True) #generator of results objects
+results = model(source=1, show=True, conf=0.4, save=True) ##generator of results objects
